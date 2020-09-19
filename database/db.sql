178615c117e56d2eb3fa333a7a7db9279226562a
Robert Garaban Garcias <67597467+Robertgaraban@users.noreply.github.com>
2020-09-19 16:07:34 -0300

    Robert 1607 - 19-09-2020

---
+++ database/db.sql
@@ -0,0 +1,15 @@
+--creating the database
+CREATE DATABASE crudnodejsmysql;
+
+
+--using the database
+use crudnodejsmysql;
+
+--creating a table
+CREATE TABLE customer (
+    id INT(6) PRIMARY KEY,
+    name
+    address
+    phone
+
+);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnecton = require('express-myconnection');
+
+
 const app = express();
 
-app.listen(3000, () => {
+
+//. settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '$Fser81',
+    port: 3306,
+    database: 'crudnodejsmysql',
+
+}, 'single'));
+
+
+app.listen(app.set('port'), () => {
     console.log('Server on port 3000');
 });
