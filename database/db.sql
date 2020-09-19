90ec581c7a51185591da524f2692ff771defb8a3
Robert Garaban Garcias <67597467+Robertgaraban@users.noreply.github.com>
2020-09-19 19:43:49 -0300

    robert 19-09-2020 databases 1

--- database/db.sql
+++ database/db.sql
@@ -1,21 +1,15 @@
---creating the database
+
 CREATE DATABASE crudnodejsmysql;
 
 
---using the database
+
 use crudnodejsmysql;
 
---creating a table
+
 CREATE TABLE customer (
    id INT(6) PRIMARY KEY,
    name VARCHAR(50) NOT NULL,
-   address VARCHAR(100),
+   address VARCHAR(100) NOT NULL,
    phone VARCHAR(15),
     
 );
-
---to chow all tables
---SHOW TABLES;
-
---to describe the table
---describe customer;
