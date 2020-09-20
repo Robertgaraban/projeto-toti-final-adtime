07a6209f7ddf6b6cbe72a19f58c015c0b8444a31
Robert Garaban Garcias <67597467+Robertgaraban@users.noreply.github.com>
2020-09-19 21:35:15 -0300

    ejs Robert 19-09-2020 rrr

--- src/app.js
+++ src/app.js
@@ -1,31 +1,37 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const mysql = require('mysql');
-const myConnecton = require('express-myconnection');
-
+const express = require('express'),
+      path = require('path'),
+      morgan = require('morgan'),
+      mysql = require('mysql'),
+      myConnection = require('express-myconnection');
 
 const app = express();
 
+// importing routes
+const customerRoutes = require('./routes/customer');
 
-//. settings
+// settings
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-
-//middlewares
+// middlewares
 app.use(morgan('dev'));
 app.use(myConnection(mysql, {
-    host: 'localhost',
-    user: 'root',
-    password: ' ',
-    port: 3306,
-    database: 'crudnodejsmysql',
-
+  host: 'localhost',
+  user: 'root',
+  password: 'contraseña',
+  port: 3306,
+  database: 'crudnodejsmysql'
 }, 'single'));
+app.use(express.urlencoded({extended: false}));
 
+// routes
+app.use('/', customerRoutes);
 
-app.listen(app.set('port'), () => {
-    console.log('Server on port 3000');
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
 });
